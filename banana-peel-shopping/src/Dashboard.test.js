4faf9f32e16c917fcfea79a5531819c9ba5b663b
riqi3 <enrique.pacudan@usjr.edu.ph>
2022-12-07 19:33:55 +0800

    updated functionalities and design

--- banana-peel-shopping/src/Dashboard.test.js
+++ banana-peel-shopping/src/Dashboard.test.js
@@ -1,21 +1,66 @@
-import { cleanup, render, screen, waitFor } from '@testing-library/react';
-import Dashboard from './Dashboard';
-import { Products } from './Dashboard';
+/* eslint-disable testing-library/await-async-utils */
+/* eslint-disable testing-library/await-async-query */
+import { cleanup, render, screen, waitFor, fireEvent } from '@testing-library/react';
+import Dashboard, {Products, Item} from './Dashboard.js';
+import "@testing-library/jest-dom/extend-expect";
+
+
+
 
 describe("Checks the Dashboard UI",()=>{
     afterEach(cleanup);
 
-    // it("has a buy button",()=>{
-    //     render(<Dashboard />)
-    //     const buybtn = screen.getByTestId("buy-btn");
-    //     expect(buybtn).toBeInTheDocument();
-    // }); // can't test a hidden button
+    it("has a quick look button", async ()=>{
+        render(<Dashboard/>)
+        fireEvent.mouseOver(screen.getByTestId("products-item1"));
+        // eslint-disable-next-line testing-library/await-async-utils
+        waitFor(() => expect(screen.findByTestId("look-btn")).toBeInTheDocument());
+    }); //passed
 
     it("has a cart button",()=>{
         render(<Dashboard />)
         const cart=screen.getByTestId("cart-btn");
         expect(cart).toBeInTheDocument();
     }) // passed
+
+    it("has a collection for products",()=>{
+        render(<Dashboard />)
+        const container=screen.getByTestId("products-container");
+        expect(container).toBeInTheDocument();
+    }) //passed
+
+    it("checks first product in the dashboard",()=>{
+        render(<Dashboard />)
+        const proItem=screen.getByTestId("products-item1");
+        expect(proItem).toBeTruthy();
+    })
+
+    it("checks all products in the dashboard",()=>{
+        render(<Dashboard />)
+        const proItem=screen.getAllByRole("products-item");
+        expect(proItem).toBeTruthy();
+    }) // passed
+
+     
+
+    it("displays a modal", async ()=>{
+        render(<Dashboard/>)
+        fireEvent.mouseEnter(screen.getByTestId("products-item1"));
+        // eslint-disable-next-line testing-library/await-async-utils
+        waitFor(() => {
+            const btn = screen.getByTestId("look-btn");
+            // eslint-disable-next-line testing-library/no-wait-for-side-effects
+            fireEvent.click(btn);
+            waitFor(() => expect(screen.getByTestId("modal")).toBeInTheDocument());
+        }); 
+    
+        
+    }); 
+    // it("has products in the dashboard",()=>{
+    //     render(<Dashboard />)
+
+        
+    // })
 
     // it("has a buy button", ()=>{
     //     render(<Dashboard />)
@@ -29,8 +74,9 @@ describe("Checks the Dashboard UI",()=>{
     //     expect(screen.getByTestId('prodcuts').length).toBe(4);
     // }) //not sure pako ani
 
-    it("checks for the buy button",async()=>{
-        render(<Dashboard />)
-        await waitFor(()=>screen.getByTestId('buy-btn', {name: /BUY/i, hidden:true}));
-    })
+    // it("has a buy button",()=>{
+    //     render(<Dashboard />)
+    //     const buy=screen.getByTestId("buy-btn");
+    //     expect(buy).toBeInTheDocument();
+    // })
 })
